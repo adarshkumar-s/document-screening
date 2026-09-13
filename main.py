8802c694d332cf07cbe0fca08865927015b6eb94
adarshkumar-s <ak8945354@gmail.com>
2026-09-13 12:47:39 +0530

    Connect saved records to Land Intelligence

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from server import app, BASE_DIR
 import land_intelligence
 from demo_land import router as demo_land_router
 from ai_governance import router as ai_approval_router
+from land_intelligence_bridge import router as land_bridge_router
 
 app.include_router(demo_land_router)
 app.include_router(ai_approval_router)
+app.include_router(land_bridge_router)
 
 
 @app.middleware("http")
